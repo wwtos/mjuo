2a50f0bd95c624876e82e72317a40f5404da405b
smj-edison <stephen.mason.jones@gmail.com>
2022-01-19 21:24:34 -0800

    Add beginnings of property editor

---
+++ src/node-editor/NumberProperty.svelte
@@ -0,0 +1,34 @@
+<script>
+export let onchange = function() {};
+export let value;
+
+let firstTime = true;
+
+$: {
+    if(!firstTime && value !== undefined) {
+        onchange(value);
+    }
+
+    if (firstTime) {
+        firstTime = false;
+    }
+}
+</script>
+
+<input type="number" style="width: 100%" bind:value={value} />
+<style>
+input {
+    height: 26px;
+    border: none;
+    margin: 0;
+    outline: none;
+    border-radius: 0;
+    box-shadow: none;
+    resize: none;
+}
+
+input:focus-visible {
+    border: 1px solid blue;
+    border-radius: 0;
+}
+</style>
---
+++ src/node-editor/PropertyEditor.svelte
@@ -0,0 +1,61 @@
+<script>
+import NumberProperty from "./NumberProperty.svelte";
+import OptionsProperty from "./OptionsProperty.svelte";
+
+export let width;
+export let height;
+</script>
+
+<div style="width: {width}px; height: {height}px">
+    <div class="container">
+        <div class="row">
+            <div class="prop-name">Number</div>
+            <div class="dividing-bar"></div>
+            <div class="prop-value"><NumberProperty value={0} /></div>
+        </div>
+        <div class="row">
+            <div class="prop-name">Stuff</div>
+            <div class="dividing-bar"></div>
+            <div class="prop-value"><OptionsProperty states={{foo: "bar", baz: "qux"}} value={"baz"} /></div>
+        </div>
+    </div>
+</div>
+
+<style>    
+.dividing-bar {
+    flex-grow: 0;
+    border-left: 1px solid rgb(216, 216, 216);
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+}
+
+.row {
+    width: 100%;
+    display: inline-flex;
+    flex-direction: row;
+    justify-content: center;
+    padding: 0;
+    border-bottom: 1px solid rgb(216, 216, 216);
+}
+
+.row:hover {
+    background-color: rgb(223, 223, 223);
+}
+
+.prop-name, .prop-value {
+    width: 50%;
+    margin: 0;
+}
+
+.prop-name {
+    text-align: right;
+    padding: 3px 6px 3px 5px;
+}
+
+.prop-value {
+    padding: 0;
+}
+</style>
