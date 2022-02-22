51409367be19cd7f56ad93a936d385660684975e
smj-edison <stephen.mason.jones@gmail.com>
2022-02-21 17:39:06 -0800

    Add custom IPC error type

---
+++ vpo-backend/ipc/src/error.rs
@@ -0,0 +1,13 @@
+use thiserror::Error;
+
+use crate::ipc_message::IPCMessage;
+
+#[derive(Error, Debug)]
+pub enum IPCError {
+    #[error("Channel send error")]
+    ChannelSendError(#[from] async_std::channel::SendError<IPCMessage>),
+    #[error("Channel receive error")]
+    ChannelReceiveError(#[from] async_std::channel::RecvError),
+    #[error("Async IO error")]
+    AsyncIOError(#[from] async_std::io::Error)
+}
