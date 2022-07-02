8abdc993cf65946f4b4e4bf9e436fed022f9cca9
smj-edison <stephen.mason.jones@gmail.com>
2022-07-02 10:52:34 -0700

    Make socket registry serializable

--- vpo-backend/src/util.rs
+++ vpo-backend/src/util.rs
@@ -1,10 +1,10 @@
 use async_std::{channel::Sender, task::block_on};
 use ipc::ipc_message::IPCMessage;
-use node_engine::graph::Graph;
+use node_engine::{graph::Graph, socket_registry::SocketRegistry, errors::NodeError};
 use serde_json::json;
 
 pub fn update_graph(graph: &Graph, to_server: &Sender<IPCMessage>) {
-    let json = graph.serialize().unwrap();
+    let json = graph.serialize_to_json().unwrap();
 
     block_on(async {
         to_server
@@ -15,4 +15,20 @@ pub fn update_graph(graph: &Graph, to_server: &Sender<IPCMessage>) {
             .await
     })
     .unwrap();
+}
+
+pub fn update_registry(registry: &mut SocketRegistry, to_server: &Sender<IPCMessage>) -> Result<(), NodeError> {
+    let json = serde_json::to_value(registry).map_err(|err| NodeError::JsonParserError(err))?;
+
+    block_on(async {
+        to_server
+            .send(IPCMessage::Json(json! {{
+                "action": "registry/updateRegistry",
+                "payload": json
+            }}))
+            .await
+    })
+    .unwrap();
+
+    Ok(())
 }
