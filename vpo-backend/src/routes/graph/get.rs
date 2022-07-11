6ed95c16d84fbe088653eadea8f8ba57453e5459
smj-edison <stephen.mason.jones@gmail.com>
2022-07-11 15:00:59 -0600

    Migrate routes

--- vpo-backend/src/routes/graph/get.rs
+++ vpo-backend/src/routes/graph/get.rs
@@ -1,6 +1,11 @@
 use async_std::channel::Sender;
 use ipc::ipc_message::IPCMessage;
-use node_engine::{errors::NodeError, node_graph::NodeGraph, socket_registry::SocketRegistry};
+use node_engine::{
+    errors::NodeError,
+    graph_manager::{GraphIndex, GraphManager},
+    socket_registry::SocketRegistry,
+};
+use rhai::Engine;
 use serde_json::{Map, Value};
 use sound_engine::SoundConfig;
 
@@ -11,11 +16,15 @@ use crate::{
 
 pub fn route(
     _message: Map<String, Value>,
-    graph: &mut NodeGraph,
+    current_graph_index: GraphIndex,
+    graph_manager: &mut GraphManager,
     to_server: &Sender<IPCMessage>,
-    _config: &SoundConfig,
+    _sound_config: &SoundConfig,
     socket_registry: &mut SocketRegistry,
+    _scripting_engine: &Engine,
 ) -> Result<Option<RouteReturn>, NodeError> {
+    let graph = &mut graph_manager.get_graph_wrapper_mut(current_graph_index).unwrap().graph;
+
     update_graph(graph, to_server);
     update_registry(socket_registry, to_server).unwrap();
 
