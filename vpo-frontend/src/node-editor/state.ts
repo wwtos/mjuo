175f0a7e1c600825608a59470253cf18b81c1a72
smj-edison <stephen.mason.jones@gmail.com>
2022-07-11 12:50:23 -0600

    Add graph behavior subject

--- vpo-frontend/src/node-editor/state.ts
+++ vpo-frontend/src/node-editor/state.ts
@@ -1,4 +1,6 @@
 import { BehaviorSubject } from "rxjs";
+import { NodeGraph } from "../node-engine/node_graph";
 import { SocketRegistry } from "../node-engine/socket_registry";
 
 export const socketRegistry: BehaviorSubject<SocketRegistry> = new BehaviorSubject(new SocketRegistry());
+export const graph: BehaviorSubject<NodeGraph | undefined> = new BehaviorSubject<NodeGraph | undefined>(undefined);
