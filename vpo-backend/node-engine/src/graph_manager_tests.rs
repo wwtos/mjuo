3744c36e99b3cd69797e6b2e9eedeaa8858aa9cb
smj-edison <stephen.mason.jones@gmail.com>
2022-07-11 12:29:40 -0600

    Add tests for graph manager

---
+++ vpo-backend/node-engine/src/graph_manager_tests.rs
@@ -0,0 +1,13 @@
+use crate::graph_manager::GraphManager;
+
+
+#[test]
+fn create_graph() {
+    let mut graph_manager = GraphManager::default();
+
+    let index = graph_manager.new_graph();
+    assert_eq!(index, 0);
+
+    let graph = graph_manager.get_graph_ref(index);
+    assert!(graph.is_some(), "Graph was none!");
+}
