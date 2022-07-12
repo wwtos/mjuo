f244b419cb260b34aefc98df15bd8e4966d47b22
smj-edison <stephen.mason.jones@gmail.com>
2022-07-12 13:39:29 -0700

    Add write to file debugging

--- vpo-backend/src/lib.rs
+++ vpo-backend/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{error::Error, io::Write};
+
 use async_std::channel::Sender;
 use ipc::ipc_message::IPCMessage;
 use node_engine::{
@@ -7,7 +9,7 @@ use node_engine::{
 };
 use rhai::Engine;
 use serde_json::Value;
-use sound_engine::SoundConfig;
+use sound_engine::{SoundConfig, constants::BUFFER_SIZE};
 
 pub mod routes;
 pub mod util;
@@ -94,4 +96,22 @@ pub fn route(
     }
 
     Ok(None)
+}
+
+pub fn write_to_file(output_file: &mut std::fs::File, data: &[f32]) -> Result<(), Box<dyn Error>> {
+    let mut data_out = [0_u8; BUFFER_SIZE * 4];
+
+    // TODO: would memcpy work here faster?
+    for i in 0..BUFFER_SIZE {
+        let num = (data[i] as f32).to_le_bytes();
+
+        data_out[i * 4] = num[0];
+        data_out[i * 4 + 1] = num[1];
+        data_out[i * 4 + 2] = num[2];
+        data_out[i * 4 + 3] = num[3];
+    }
+
+    output_file.write_all(&data_out)?;
+
+    Ok(())
 }
