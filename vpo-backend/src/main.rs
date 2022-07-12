038a471464295860775d797191f579b907db18fd
smj-edison <stephen.mason.jones@gmail.com>
2022-07-12 13:39:03 -0700

    Add ALSA backend

--- vpo-backend/src/main.rs
+++ vpo-backend/src/main.rs
@@ -18,16 +18,17 @@ use node_engine::nodes::variants::NodeVariant;
 use node_engine::socket_registry::SocketRegistry;
 use rhai::Engine;
 use serde_json::json;
+use sound_engine::backend::alsa::AlsaAudioBackend;
 use sound_engine::backend::alsa_midi::AlsaMidiClientBackend;
 use sound_engine::backend::MidiClientBackend;
-use sound_engine::backend::{pulse::PulseClientBackend, AudioClientBackend};
+use sound_engine::backend::{AudioClientBackend};
 use sound_engine::constants::{BUFFER_SIZE, SAMPLE_RATE};
 use sound_engine::SoundConfig;
 
 use ipc::ipc_server::IPCServer;
 use sound_engine::midi::messages::MidiData;
 use sound_engine::midi::parse::MidiParser;
-use vpo_backend::{route, RouteReturn};
+use vpo_backend::{route, RouteReturn, write_to_file};
 
 fn start_ipc() -> (Sender<IPCMessage>, Receiver<IPCMessage>) {
     let (to_server, from_main) = unbounded::<IPCMessage>();
@@ -74,30 +75,30 @@ fn handle_msg(
 
             if route_result.should_reindex_graph {
                 graph_manager.recalculate_traversal_for_graph(*current_graph_index);
-
-                let nodes_to_update = {
-                    let graph = &graph_manager.get_graph_wrapper_ref(*current_graph_index).unwrap().graph;
-
-                    // TODO: this is naive, keep track of what nodes need their defaults updated
-                    graph
-                        .get_nodes()
-                        .iter()
-                        .enumerate()
-                        .filter_map(|(i, node)| {
-                            if let PossibleNode::Some(_, generation) = node {
-                                Some(NodeIndex {
-                                    index: i,
-                                    generation: *generation,
-                                })
-                            } else {
-                                None
-                            }
-                        })
-                        .collect::<Vec<NodeIndex>>()
-                };
-
-                graph_manager.update_traversal_defaults(*current_graph_index, nodes_to_update);
             }
+
+            let nodes_to_update = {
+                let graph = &graph_manager.get_graph_wrapper_ref(*current_graph_index).unwrap().graph;
+
+                // TODO: this is naive, keep track of what nodes need their defaults updated
+                graph
+                    .get_nodes()
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(i, node)| {
+                        if let PossibleNode::Some(_, generation) = node {
+                            Some(NodeIndex {
+                                index: i,
+                                generation: *generation,
+                            })
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<NodeIndex>>()
+            };
+
+            graph_manager.update_traversal_defaults(*current_graph_index, nodes_to_update);
         }
         Err(err) => {
             let err_str = err.to_string();
@@ -116,7 +117,7 @@ fn handle_msg(
 }
 
 fn connect_backend() -> Result<Box<dyn AudioClientBackend>, Box<dyn Error>> {
-    let mut backend: Box<dyn AudioClientBackend> = Box::new(PulseClientBackend::new());
+    let mut backend: Box<dyn AudioClientBackend> = Box::new(AlsaAudioBackend::new());
     backend.connect()?;
 
     Ok(backend)
@@ -147,6 +148,8 @@ fn get_midi(midi_backend: &mut Box<dyn MidiClientBackend>, parser: &mut MidiPars
 
 fn main() -> Result<(), Box<dyn Error>> {
     let (to_server, from_server) = start_ipc();
+
+    let mut output_file = std::fs::File::create("audio.raw").unwrap();
 
     let mut socket_registry = SocketRegistry::new();
     SocketType::register_defaults(&mut socket_registry);
@@ -246,14 +249,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 
             is_first_time = false;
         }
-
+        
         backend.write(&buffer)?;
+        //write_to_file(&mut output_file, &buffer)?;
 
         let now = Instant::now() - start;
-        let sample_duration = Duration::from_secs_f64(1.0 / (SAMPLE_RATE as f64 / BUFFER_SIZE as f64));
+        let sample_duration = Duration::from_secs_f64(BUFFER_SIZE as f64 / SAMPLE_RATE as f64);
         let buffer_time = Duration::from_secs_f64((buffer_index as f64) * sample_duration.as_secs_f64());
 
-        if !(now > buffer_time || buffer_time - now < Duration::from_secs_f64(0.3)) {
+        // println!("now: {:?}, now (buffer): {:?}", now, buffer_time);
+
+        if !(now > buffer_time || buffer_time - now < sample_duration * 2) {
             thread::sleep(sample_duration);
         }
 
