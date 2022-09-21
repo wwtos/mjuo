997f3d8eb933597bfe64ec3885c40b5b0f1c2def
smj-edison <stephen.mason.jones@gmail.com>
2022-09-20 17:14:41 -0700

    More work on building

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,6 +12,10 @@ mkdir -p build
 
 # build the backend
 cd vpo-backend
-cargo build --profile release-x86_64-pc-windows-gnu --target x86_64-pc-windows-gnu # windows
+#cargo build --profile release-x86_64-pc-windows-gnu --target x86_64-pc-windows-gnu # windows
 cargo build --profile release-x86_64-unknown-linux-gnu --target x86_64-unknown-linux-gnu # linux
 
+# build the frontend
+cd "$working_directory"
+npm install
+npx electron-packager . mjuo-frontend --overwrite --platform=linux --arch=x64 --icon=assets/icons/png/1024x1024.png --prune=true --out=../build
