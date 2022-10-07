41c517c0e90c361e5ebd79342fe77c3560dcf4b0
smj-edison <stephen.mason.jones@gmail.com>
2022-10-06 21:03:36 -0700

    Refactor messages.ts

---
+++ vpo-frontend/src/sound-engine/midi/messages.ts
@@ -0,0 +1,35 @@
+import { makeTaggedUnion, MemberType, none } from "safety-match";
+import { DiscriminatedUnion } from "../../util/discriminated-union";
+
+export interface Timecode {
+    hours: number,
+    minutes: number,
+    seconds: number,
+}
+
+export type SystemCommonMessageData = DiscriminatedUnion<"variant", {
+    SystemExclusive: { content: { id: Uint8Array, message: Uint8Array }},
+    QuarterFrame: { content: { rate: number, time: Timecode }},
+}>;
+
+export type SystemRealtimeMessageData = DiscriminatedUnion<"variant", {
+    TimingClock: {},
+    Start: {},
+    Continue: {},
+    Stop: {},
+    ActiveSensing: {},
+    Reset: {},
+}>;
+
+export type MidiData = DiscriminatedUnion<"variant", {
+    NoteOff: { content: { channel: number, note: number, velocity: number }},
+    NoteOn: { content: { channel: number, note: number, velocity: number }},
+    Aftertouch: { content: { channel: number, note: number, pressure: number }},
+    ControlChange: { content: { channel: number, controller: number, value: number }},
+    ProgramChange: { content: { channel: number, patch: number }},
+    ChannelAftertouch: { content: { channel: number, pitchBend: number }},
+    PitchBend: { content: { channel: number, bend: number }},
+    SystemCommonMessage: { content: { data: SystemCommonMessageData }},
+    SystemRealtimeMessage: { content: { data: SystemRealtimeMessageData }},
+    MidiNone: {}
+}>;
